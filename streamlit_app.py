59f9d931bf30974afcf7aa4265a195cbdb0eee2d
Sathya Karthik <118291901+Sathya354@users.noreply.github.com>
2022-11-15 10:08:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('OMEGA 3 and Blueberry Oatmeals')
+streamlit.text('Kale, Spinach and Smoothie')
+streamlit.text('Hard-Boil Free-Range Egg)
