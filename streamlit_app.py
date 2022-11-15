d3e9f2be30970b03ed6f4cd40b0f9428bf73d5cb
Sathya Karthik <118291901+Sathya354@users.noreply.github.com>
2022-11-15 10:10:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('OMEGA 3 and Blueberry Oatmeals')
 streamlit.text('Kale, Spinach and Smoothie')
 streamlit.text('Hard-Boil Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
